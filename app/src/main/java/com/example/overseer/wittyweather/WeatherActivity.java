70a88caf3457618cda071e61e47077b14fd5c731
William Mabry <wmabry2@radford.edu>
2017-03-20 15:07:26 -0400

    Android Build files
    The initial build files for an android project weather app.

---
+++ app/src/main/java/com/example/overseer/wittyweather/WeatherActivity.java
@@ -0,0 +1,56 @@
+package com.example.overseer.wittyweather;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
+
+public class WeatherActivity extends AppCompatActivity {
+
+
+    TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField, title_Field;
+    Typeface weatherFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_weather);
+
+
+        //weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
+
+        cityField = (TextView)findViewById(R.id.city_field);
+        updatedField = (TextView)findViewById(R.id.updated_field);
+        detailsField = (TextView)findViewById(R.id.details_field);
+        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView)findViewById(R.id.humidity_field);
+        pressure_field = (TextView)findViewById(R.id.pressure_field);
+        weatherIcon = (TextView)findViewById(R.id.weather_icon);
+        //title_Field = (TextView)findViewById(R.id.title_field);
+        //weatherIcon.setTypeface(weatherFont);
+
+
+        Utility.placeIdTask asyncTask = new Utility.placeIdTask(new Utility.AsyncResponse() {
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
+
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updatedOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                humidity_field.setText("Humidity: "+weather_humidity);
+                pressure_field.setText("Pressure: "+weather_pressure);
+                //title_Field.setText(titleField);
+                //weatherIcon.setText(Html.fromHtml(weather_iconText));
+
+            }
+        });
+        asyncTask.execute("37.131792", "-80.576448"); //  asyncTask.execute("Latitude", "Longitude")
+
+
+
+    }
+
+
+}
